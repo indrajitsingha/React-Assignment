a60c12e1c6cf1789375ff6645b4b2ec5ab03c958
indrajitsingha <72645452+indrajitsingha@users.noreply.github.com>
2023-09-10 21:26:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//base:"/React-Assignment/",
 export default defineConfig({
-  base:"/React-Assignment/",
   plugins: [react()],
 })
